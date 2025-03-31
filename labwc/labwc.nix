88042cdd554418cceb1a157e5d6c8384940a136d
mzamorano <martin@mzamorano.com>
2025-03-30 18:04:26 -0700

    apply theme to Labwc

--- labwc/labwc.nix
+++ labwc/labwc.nix
@@ -1,6 +1,13 @@
 { config, lib, pkgs, ... }:
 
 {
+  xdg.dataFile = {
+    "themes" = {
+      source = ../vendor/labwc-themes;
+      recursive = true;
+    };
+  };
+
   xdg.configFile = {
     "labwc/rc.xml".source = ./rc.xml;
     "labwc/autostart".source = ./autostart;
