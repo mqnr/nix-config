3cab6e143f0abaf432fa1e441f8b2b4f4d4e1698
mzamorano <martin@mzamorano.com>
2025-04-09 14:21:07 -0700

    add global gitignore

--- home/git.nix
+++ home/git.nix
@@ -14,6 +14,17 @@
       "last" = "log -1 HEAD";
       "lg" = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
     };
+    ignores = [
+      ".idea"
+      ".direnv"
+      ".envrc"
+      "*~"
+      ".fuse_hidden*"
+      ".directory"
+      ".Trash-*"
+      ".nfs*"
+      "nohup.out"
+    ];
     extraConfig = {
       init.defaultBranch = "main";
       color.ui = "auto";
