30bc30a32273b0eda3f3b47f55b06ad891a71bc4
mzamorano <martin@mzamorano.com>
2025-05-27 17:24:02 -0700

    fix private path

--- modules/shared/common.nix
+++ modules/shared/common.nix
@@ -1,7 +1,7 @@
 { lib, ... }:
 
 {
-  imports = lib.optional (builtins.pathExists ../modules/shared/location-private.nix) ../modules/shared/location-private.nix;
+  imports = lib.optional (builtins.pathExists ../../modules/shared/location-private.nix) ../../modules/shared/location-private.nix;
 
   nixpkgs.config.allowUnfree = true;
 }
