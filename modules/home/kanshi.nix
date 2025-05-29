3f652aac005379fe55b25b143ea5818d586796d0
mzamorano <martin@mzamorano.com>
2025-05-28 17:56:03 -0700

    add external monitor to Kanshi config

--- modules/home/kanshi.nix
+++ modules/home/kanshi.nix
@@ -1,17 +1,24 @@
-{ config, lib, pkgs, ... }:
+{ ... }:
 
 {
   services.kanshi = {
     enable = true;
-    settings = let laptop = "eDP-1"; in [
+    settings = let laptop = "eDP-1"; exmonitor = "HDMI-A-1"; in [
       { output.criteria = laptop;
         output.scale = 1.25;
       }
+      { output.criteria = exmonitor;
+        output.mode = "1920x1080@100Hz";
+      }
       { profile.name = "undocked";
         profile.outputs = [
-          {
-            criteria = laptop;
-          }
+          { criteria = laptop; }
+        ];
+      }
+      { profile.name = "docked";
+        profile.outputs = [
+          { criteria = laptop; status = "disable"; }
+          { criteria = exmonitor; }
         ];
       }
     ];
