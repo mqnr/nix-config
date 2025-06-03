1a54ed12d894c976d240784188cec0f1093d36b8
mzamorano <martin@mzamorano.com>
2025-06-03 08:47:48 -0700

    waybar changes

--- modules/linux/waybar/default.nix
+++ modules/linux/waybar/default.nix
@@ -9,18 +9,51 @@
       modules-left = [
         "niri/workspaces"
       ];
+      "niri/workspaces" = {
+        format = "{icon}";
+        format-icons = {
+          active = " ";
+          default = " ";
+        };
+      };
       modules-center = [
         "niri/window"
       ];
       modules-right = [
         "tray"
         "battery"
-        "wireplumber"
+        "pulseaudio"
         "clock"
       ];
+      pulseaudio = {
+          format = "{format_source} {icon} {volume}%";
+          format-muted = "{format_source} 󰸈 {volume}%";
+          format-bluetooth = "{format_source} 󰋋 󰂯 {volume}%";
+          format-bluetooth-muted = "{format_source} 󰟎 󰂯 {volume}%";
+          format-source = "󰍬";
+          format-source-muted = "󰍭";
+          format-icons.default = [ "󰕿" "󰖀" "󰕾" ];
+      };
+      battery = {
+        format = "{icon} {capacity}%";
+        format-charging = "󰂄 {capacity}%";
+        format-plugged = "󰂄 {capacity}%";
+        format-icons = [ "󰁺" "󰁻" "󰁼" "󰁽" "󰁾" "󰁿" "󰂀" "󰂁" "󰂂" "󰁹" ];
+        states = {
+          warning  = 30;
+          critical = 15;
+        };
+      };
       clock = {
         format = "{:%a %Y-%m-%d %H:%M}";
-        tooltip = false;
+        tooltip-format = "<tt><small>{calendar}</small></tt>";
+        calendar = {
+          weeks-pos = "right";
+          format = {
+            weeks = "| W{}";
+            today = "<b><u>{}</u></b>";
+          };
+        };
       };
       tray = {
         icon-size = 17;
