2a8056415f65efcc521992b2b72f8e6f9683eb99
mzamorano <martin@mzamorano.com>
2025-06-21 17:13:19 -0700

    add some Fish aliases

--- modules/common/fish.nix
+++ modules/common/fish.nix
@@ -3,9 +3,21 @@
 {
   programs.fish = {
     enable = true;
+
     interactiveShellInit = ''
       set fish_greeting # Disable greeting
       direnv hook fish | source
     '';
+
+    shellAliases = {
+      g = "git";
+      o = "bat --plain";
+      p = "bat --plain --paging=auto";
+
+      "..."    = "cd ../..";
+      "...."   = "cd ../../..";
+      "....."  = "cd ../../../..";
+      "......" = "cd ../../../../..";
+    };
   };
 }
