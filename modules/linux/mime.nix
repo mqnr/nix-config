5d46e22a7cc91ea5220ba1e7e865aecec7d93c3e
mzamorano <martin@mzamorano.com>
2025-06-27 17:08:59 -0700

    set Firefox as default browser

--- modules/linux/mime.nix
+++ modules/linux/mime.nix
@@ -1,12 +1,26 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
+  # TODO: move this somewhere else?
+  home.sessionVariables = let browserPath = "${pkgs.firefox}/bin/firefox"; in {
+    BROWSER         = browserPath;
+    DEFAULT_BROWSER = browserPath;
+  };
+
   xdg.mimeApps = {
     enable = true;
     defaultApplications = let
+      browser        = [ "firefox.desktop" ];
       documentViewer = [ "org.kde.okular.desktop" ];
       imageViewer    = [ "viewnior.desktop" ];
     in {
+      # Default browser
+      "text/html"                = browser;
+      "x-scheme-handler/http"    = browser;
+      "x-scheme-handler/https"   = browser;
+      "x-scheme-handler/about"   = browser;
+      "x-scheme-handler/unknown" = browser;
+
       # PDF files in Okular
       "application/pdf"   = documentViewer;
       "application/x-pdf" = documentViewer;
