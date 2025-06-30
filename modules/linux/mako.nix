bc8a60ccad64f1b0af8eea283c18dcf1fdf67571
mzamorano <martin@mzamorano.com>
2025-06-30 16:25:28 -0700

    set default timeout for Mako

--- modules/linux/mako.nix
+++ modules/linux/mako.nix
@@ -1,5 +1,11 @@
 { config, lib, pkgs, ... }:
 
 {
-  services.mako.enable = true;
+  services.mako = {
+    enable = true;
+
+    settings = {
+      default-timeout = 120000; # 2 minutes
+    };
+  };
 }
