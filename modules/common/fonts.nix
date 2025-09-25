651f2c32c6ae577259a7e2ccbb2d4c11a5ae1236
mzamorano <martin@mzamorano.com>
2025-09-25 04:05:35 -0700

    feat: add additional fonts

--- modules/common/fonts.nix
+++ modules/common/fonts.nix
@@ -9,6 +9,9 @@ lib.mkIf config.isPC {
   home-manager.sharedModules = [
     {
       home.packages = [
+        pkgs.noto-fonts
+        pkgs.noto-fonts-cjk-sans
+        pkgs.noto-fonts-emoji
         pkgs.aporetic
         pkgs.nerd-fonts.jetbrains-mono
       ];
