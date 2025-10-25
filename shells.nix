4334aad2cc6652839aefed846bce0ce4b1e18a81
mzamorano <martin@mzamorano.com>
2025-10-25 01:03:08 -0700

    modify shells

--- shells.nix
+++ shells.nix
@@ -1,10 +1,14 @@
 { pkgs, ... }:
 
 {
+  programs.zsh.enable = true;
+  programs.bash.enable = true;
   programs.fish.enable = true;
   programs.nushell.enable = true;
 
   environment.shells = [
+    pkgs.zsh
+    pkgs.bashInteractive
     pkgs.fish
     pkgs.nushell
   ];
