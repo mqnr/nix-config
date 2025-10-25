ca343578a8235cabbb08262eae7e864bf151cb75
mzamorano <martin@mzamorano.com>
2025-10-25 01:20:36 -0700

    come on

--- shells.nix
+++ shells.nix
@@ -1,14 +1,9 @@
 { pkgs, ... }:
 
 {
-  programs.zsh.enable = true;
-  programs.bash.enable = true;
   programs.fish.enable = true;
-  programs.nushell.enable = true;
 
   environment.shells = [
-    pkgs.zsh
-    pkgs.bashInteractive
     pkgs.fish
     pkgs.nushell
   ];
