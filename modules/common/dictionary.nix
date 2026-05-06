e9649c355d97b8f88af1f05003c5ada28f8f42e4
Martín Zamorano <martin@mzamorano.com>
2026-05-06 16:35:46 -0700

    add dictionaries

--- modules/common/dictionary.nix
+++ modules/common/dictionary.nix
@@ -9,7 +9,6 @@ lib.mkIf config.isPC {
   home-manager.sharedModules = [
     {
       home.packages = [
-        pkgs.enchant
         pkgs.hunspellDicts.en_US-large
         pkgs.hunspellDicts.es_MX
       ];
