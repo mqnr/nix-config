bd89c16206ba0bbf0408f549416daa56896a93df
Martín Zamorano <martin@mzamorano.com>
2026-05-06 16:38:12 -0700

    create .dir-locals.el to hold Emacs Jinx dictionary words

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((jinx-dir-local-words . "Apheleia Eglot Elpaca Vertico basedpyright capf defvar dolist elpa elpaca flyspell jdtls jimeh lsp modified multimarkdown nixd pyright subdir treesit wgrep"))))
